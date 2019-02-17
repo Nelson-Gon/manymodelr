4b06229a8a8d750cf48a2dc5686e4671c8b33698
NelsonGon <42792514+Nelson-Gon@users.noreply.github.com>
2019-02-17 14:24:21 +0800

    Updated Functions

--- R/modeleR.R
+++ R/modeleR.R
@@ -1,19 +1,31 @@
-#' Perform several kinds of models in one click
+#' Perform several kinds of models in one function
 #' @importFrom stats "as.formula"  "complete.cases" "setNames"
+#' @importFrom stats "predict"
 #' @param df The data for which analysis is required
 #' @param yname The dependent variable
 #' @param xname Theindependent variable. Supports formulae x1+x2+...
 #' @param modeltype Currently one of lm and aov. Other models may work with inaccuracies
 #' @param na.rm Logical. Should missing values be removed from analysis?
+#' @param new_data A data.frame object for which new predictions are to be made
 #' @param ... Additional arguments to the modeltype
 #' @return A list containing summary stats and a data.frame object of some stats.
 #' @details This function provides a friendly way to perform any kind of model in one line.
+#' The model uses the inbuilt R functions aov and lm to make the predictions.
+#' @references
+#' Chambers, J. M. (1992) Linear models. Chapter 4 of Statistical Models in S eds J. M. Chambers and T. J. Hastie, Wadsworth & Brooks/Cole.
+#'
+#'Wilkinson, G. N. and Rogers, C. E. (1973). Symbolic descriptions of factorial models for analysis of variance. Applied
+#'Statistics, 22, 392-399. doi: 10.2307/2346786.
 #' @examples
+#' iris1<-iris[1:60,]
+#' iris2<-iris[60:nrow(iris),]
+#' modeleR(iris1,Sepal.Length,Petal.Length,
+#'          lm,na.rm=TRUE,iris2)
 #' \dontrun{
-#' modeleR(iris,Sepal.Length,Petal.Length,lm,na.rm=T)
+#' modeleR(iris,Sepal.Length,Petal.Length,lm,na.rm=TRUE)
 #' }
 #' @export
-modeleR<-function(df,yname,xname,modeltype,na.rm=F,...){
+modeleR<-function(df,yname,xname,modeltype,na.rm=F,new_data,...){
   #Takes data as df,yname as y variable,xname as
   #x variables modletype of either lm or glm
   yname<-deparse(substitute(yname))
@@ -22,14 +34,13 @@ modeleR<-function(df,yname,xname,modeltype,na.rm=F,...){
   formula1<-as.formula(paste(yname,"~",xname))
   if(!modeltype%in%c("lm","aov")) stop("Model Type Not Suitable.")
   if(modeltype=="lm"){
-  lm.fit<-do.call(modeltype,list(data=quote(df),formula1,...))
-  m<-summary(lm.fit)
-  df<-as.data.frame(sapply(m,"[[",1)[c(3,4,8,9,10)])
-  df<-cbind(df,Explanatory=xname,Response=yname)
-  names(df)<-gsub("^r","R",names(df))
- return(
-   list(DataFrame=df,Summary_data=m)
- )
+    lm.fit<-do.call(modeltype,list(data=quote(df),formula1,...))
+    m<-summary(lm.fit)
+    df<-as.data.frame(sapply(m,"[[",1)[c(3,4,8,9,10)])
+    df<-cbind(df,Explanatory=xname,Response=yname)
+    names(df)<-gsub("^r","R",names(df))
+    list(DataFrame=df,Summary_data=m,
+         Predictions=data.frame(Predictions=as.data.frame(predict(lm.fit))))
   }
   else if(modeltype=="aov" & na.rm==T){
     lm.fit<-do.call(modeltype,list(data=quote(df),formula1,...))
@@ -44,8 +55,9 @@ modeleR<-function(df,yname,xname,modeltype,na.rm=F,...){
     df1$Value=round(df1$Value,3)
     df1<-df1[,c("Var","Value")]
     df1<-na.omit(df1)
-     return(list(Summary_data=m,Stats=df1))
-      #tibble::as.tibble(do.call(cbind,split(df1,substr(df$Var,1,1))))
+    list(DataFrame=df,Summary_data=m,Predictions=data.frame(Predictions=
+                                                              as.data.frame(predict(lm.fit))))
+    #tibble::as.tibble(do.call(cbind,split(df1,substr(df$Var,1,1))))
   }
   else if(modeltype=="aov" & na.rm==F){
     lm.fit<-do.call(modeltype,list(data=quote(df),formula=formula1,...))
@@ -57,8 +69,10 @@ modeleR<-function(df,yname,xname,modeltype,na.rm=F,...){
     df1<-dplyr::as_tibble(df1)
     df1$Var<-rownames(df1)
     df1$Value=round(df1$Value,3)
-       df1[,c("Value","Var")]
-    return(list(Summary_data=m,Stats=df1))
+    df1[,c("Value","Var")]
+    list(DataFrame=df,Summary_data=m,Predictions=data.frame(Predictions=
+                                                              as.data.frame(predict(lm.fit))))
     #tibble::as.tibble(do.call(cbind,split(df1,substr(df$Var,1,1))))
   }
 }
+
--- R/multi_model.R
+++ R/multi_model.R
@@ -1,39 +1,70 @@
-#' Perform many models in a single command
+#' Simultaneously train and predict on new data.
 #' @description This function provides a convenient way to train several model types.
+#' It allows a user to predict on new data and depending on the  metrics, the user is able to decide which model
+#' predictions to finally use. The models are built based on Max Kuhn's models in the caret package.
 #' @param df The data holding the training dataset
 #' @param yname The outcome variable
 #' @param xname The predictor variable(s)
-#' @param method One of the many methods available in caret
+#' @param method A vector containing methods to be used as defined in the caret package
 #' @param metric One of several metrics. Accuraciy,RMSE,MAE,etc
 #' @param control See caret ?trainControl for details.
+#' @param newdata A data set to validate the model or for which predictions are required
 #' @param ... Other arguments to caret's train function
 #' @importFrom magrittr "%>%"
-#' @importFrom stats "as.formula"  "complete.cases" "setNames" "na.omit"
+#' @importFrom stats "as.formula"  "complete.cases" "setNames" "na.omit" "predict"
 #' @importFrom dplyr "arrange" "desc"
 #' @import caret
-#' @return A data.frame object of summary statistics of the model types used
+#' @import e1071
+#' @return A list containing two objects. A tibble containing a summary of the metrics per model
+#' and a tibble containig predicted values
 #' @details Most of the details of the parameters can be found in the caret package documentation.
-#' This function is meant to help in exploratory analysis to make an informed choice of the best models
+#' This function is meant to help in exploratory analysis to
+#' make an informed choice of the best models
+#' @references
+#' Kuhn (2014), "Futility Analysis in the Cross-Validation of Machine Learning Models" http://arxiv.org/abs/1405.6974,
+#'
+#' Kuhn (2008), "Building Predictive Models in R Using the caret" (http://www.jstatsoft.org/article/view/v028i05/v28i05.pdf)
 #' @examples
-#' \dontrun{
+#' library(caret)
+#' train_set<-createDataPartition(iris$Species,p=0.8,list=FALSE)
+#' valid_set<-iris[-train_set,]
+#' train_set<-iris[train_set,]
 #' ctrl<-trainControl(method="cv",number=5)
-#' multi_model(iris,"Species",".",c("rf","svmRadial","knn"),"Accuracy",ctrl)
-#' my_grid<-expand.grid(.mtry=c(4:12))
-#' multi_model(iris,"Species",".",c("rf"),"Accuracy",ctrl,tuneGrid=my_grid)
+#' set.seed(233)
+#' m<-multi_model_1(train_set,"Species",".",c("knn","rpart"),
+#'                 "Accuracy",ctrl,newdata =valid_set)
+#' m$Predictions
+#' m$Metrics
+#' \dontrun{
+#'library(caret)
+#'trainda<-createDataPartition(iris$Sepal.Length,p=0.8,list=F)
+#'valid_da<-iris[-trainda,]
+#'trainda<-iris[trainda,]
+#'ctrl<-trainControl(method="cv",number=5)
+#'set.seed(233)
+#'m<-multi_model_1(trainda,"Sepal.Length",".",c("knn","rf","xgbTree","gbm"),
+#'                 "RMSE",ctrl,newdata =valid_da,verbose=F)
+#'m$Metrics
 #' }
 #' @export
- multi_model<-function(df,yname,xname,method,metric,control,...){
+multi_model_1<-function(df,yname,xname,method,metric,control,...,newdata){
   #yname<-deparse(substitute(yname))
   #xname<-unlist(sapply(substitute(...()),deparse))
   df<-df
   methods1<-method
- f1<-as.formula(paste0(yname,"~",xname))
- fit<-lapply(methods1,function(met){set.seed(233)
-   fit<-do.call("train",list(data=quote(df),f1,metric=metric,trControl=control,
-                             method=met,...))})
+  f1<-as.formula(paste0(yname,"~",xname))
+  fit<-lapply(methods1,function(met){set.seed(233)
+    fit<-do.call("train",list(data=quote(df),f1,metric=metric,trControl=control,
+                              method=met,...))})
 
- res<-plyr::ldply(fit,function(mod) data.frame(mod$results,
-                                        Type=mod$modelInfo$tags[1:nrow(mod$results)]))
- dplyr::arrange(res,dplyr::desc(res$Accuracy))
- }
+  preds<-lapply(fit,function(x) predict(x,newdata))
+  res<-as.data.frame(sapply(preds,function(x) do.call(unlist,list(x))))
+  names(res)<-method
+  new_metric<-tolower(metric)
+  new_metric<-eval(parse(text = paste0("Metrics::", new_metric)))
+  print("Returning Metrics")
+  metrics<-lapply(res, function(x) new_metric(newdata[,yname],x))
+  Results<-list(Metrics=tibble::as_tibble(metrics),
+                Predictions=tibble::as_tibble(res))
+}
 
