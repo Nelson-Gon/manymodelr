bac79281784a2a978668d98619448a25d291e513
NelsonGon <42792514+Nelson-Gon@users.noreply.github.com>
2019-05-22 16:12:36 +0800

    refactor. Improve robustness

--- R/select_percentile.R
+++ R/select_percentile.R
@@ -1,14 +1,43 @@
 #' Get the row corresponding to a given percentile
 #' @param df The dataframe for which a percentile is required
-#' @param n The percentile required eg 10th percentile
-#' @return A dataframe showing the row corresponding to a given percentile
-#' @examples
-#' select_percentile(iris,5)
-#'\dontrun{
-#'select_percentile(mtcars,1)
-#'}
+#' @param percentile The percentile required eg 10 percentile
+#' @param get_all Logical. Should all variables be used? Currenty only supports
+#' using all variables.
+#' @param ascend Logical. Should the data be arranged in ascending order? Defaults
+#' to FALSE.
+#' @return A dataframe showing the row corresponding to the required percentile.
+#'  @examples
+#' select_percentile(iris,5,)
 #'@export
-select_percentile<-function(df,n){
-  nth <- ((100-n)/nrow(df)) * 100
-  return(df[nth, ])
+select_percentile<-function(df,percentile,get_all=T,ascend=FALSE){
+  # Returns the value corresponding to a percentile
+  # Returns mean values if the position of the percentile is  whole number
+  # Values are sorted in ascending order. You can change this
+  # by setting ascend to TRUE.
+
+  if(!is.data.frame(df)){
+    stop("Please provide a `data.frame` object. Other
+                            data types are not (yet) supported.")
+
+  }
+  numerics <- Filter(is.numeric,df)
+  sort_by<- which(names(df)==names(numerics)[1])
+  if(get_all==TRUE){
+    sort_by<-names(numerics)[1]
+    ordered_df<-df[sort(df[,sort_by],decreasing = ascend,
+                        index.return=T)[[2]],]
+  }
+  #return(ordered)
+  nth<-(percentile / 100) * length(ordered_df)
+  #return(nth)
+  print(noquote(paste0("### Ordered by ",
+                       names(numerics)[1]," ###")))
+
+  if(nchar(nth)<=1){
+    return(mean(ordered_df[nth:(nth+1)]))
+  }
+  else{
+    ordered_df[ceiling(nth),]
+  }
+
 }
