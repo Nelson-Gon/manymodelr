11c29c32644390bcb2e13354f41be74bb0aa0cea
Nelson-Gon <gonzabato@hotmail.com>
2019-06-23 23:03:42 +0800

    Update percentile, issue with devtools: https://github.com/r-lib/pkgbuild/issues/58

--- R/select_percentile.R
+++ R/select_percentile.R
@@ -3,13 +3,13 @@
 #' @param percentile The percentile required eg 10 percentile
 #' @param get_all Logical. Should all variables be used? Currenty only supports
 #' using all variables.
-#' @param ascend Logical. Should the data be arranged in ascending order? Defaults
+#' @param descend Logical. Should the data be arranged in descending order? Defaults
 #' to FALSE.
 #' @return A dataframe showing the row corresponding to the required percentile.
 #'  @examples
 #' select_percentile(iris,5,)
 #'@export
-select_percentile<-function(df,percentile,get_all=T,ascend=FALSE){
+select_percentile<-function(df,percentile,get_all=T,descend=FALSE){
   # Returns the value corresponding to a percentile
   # Returns mean values if the position of the percentile is  whole number
   # Values are sorted in ascending order. You can change this
@@ -24,7 +24,7 @@ select_percentile<-function(df,percentile,get_all=T,ascend=FALSE){
   sort_by<- which(names(df)==names(numerics)[1])
   if(get_all==TRUE){
     sort_by<-names(numerics)[1]
-    ordered_df<-df[sort(df[,sort_by],decreasing = ascend,
+    ordered_df<-df[sort(df[,sort_by],decreasing = descend,
                         index.return=T)[[2]],]
   }
   #return(ordered)
