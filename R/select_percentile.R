f48d95e15a1ff5ad96e1a5b27590e135e54d2481
Nelson-Gon <gonzabato@hotmail.com>
2019-06-23 23:30:17 +0800

    Explicitly mention return values, data type.

--- R/select_percentile.R
+++ R/select_percentile.R
@@ -1,19 +1,25 @@
 #' Get the row corresponding to a given percentile
-#' @param df The dataframe for which a percentile is required
+#' @param df A `data.frame` object for which a percentile is required. Other data structures are not yet supported.
 #' @param percentile The percentile required eg 10 percentile
 #' @param get_all Logical. Should all variables be used? Currenty only supports
 #' using all variables.
 #' @param descend Logical. Should the data be arranged in descending order? Defaults
 #' to FALSE.
+#' @details 
+#' Returns the value corresponding to a percentile. 
+#' Returns mean values if the position of the percentile is  whole number.
+#' Values are sorted in ascending order. You can change this by setting descend to TRUE.
+#' 
+#' 
 #' @return A dataframe showing the row corresponding to the required percentile.
-#'  @examples
+#' @examples
 #' select_percentile(iris,5)
 #'@export
 select_percentile<-function(df,percentile,get_all=T,descend=FALSE){
   # Returns the value corresponding to a percentile
   # Returns mean values if the position of the percentile is  whole number
   # Values are sorted in ascending order. You can change this
-  # by setting ascend to TRUE.
+  # by setting descend to TRUE.
 
   if(!is.data.frame(df)){
     stop("Please provide a `data.frame` object. Other
