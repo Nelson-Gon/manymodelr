c9e1e7f35f77ec7a23f69a7350495de91a0afd0c
Nelson-Gon <gonzabato@hotmail.com>
2019-07-22 15:40:42 +0800

    redeal with NAs in mode

--- R/get_mode.R
+++ R/get_mode.R
@@ -1,6 +1,7 @@
 
 #'A convenience function that returns the mode
-#'@param x  The dataframe or vector for which the mode is required
+#'@param x  The dataframe or vector for which the mode is required.
+#'@param na.rm Logical. Should NAs be removed from analysis? Deafults to TRUE.
 #'@return a data.frame or vector showing the mode of the variable(s)
 #'@details Useful when used together with get_stats in a pipe fashion.
 #'These functions are for exploratory data analysis
@@ -8,6 +9,7 @@
 #' The function is currently slow for greater than 300,000 rows. It may take up to a minute.
 #' Getting the mode for charcater/ factor columns is also currently unsupported. It 
 #' may work with inaccuracies. By default, NAs are discarded. 
+#' @importFrom stats na.omit
 #' @examples
 #'test<-c(1,2,3,3,3,3,4,5)
 #'test2<-c(455,7878,908981,NA,456,455,7878,7878,NA)
@@ -15,49 +17,57 @@
 #'get_mode(test2)
 #'\dontrun{
 #'mtcars %>%
-#'get_stats(get_mode)
-#'get_stats(mtcars,get_mode)}
+#'get_data_Stats(get_mode)
+#'get_data_Stats(mtcars,get_mode)}
 #'@export
-get_mode <- function(x){
+get_mode <- function(x,na.rm=TRUE){
   UseMethod("get_mode")
 }
 #' @export
-get_mode.numeric<-function(x){
+get_mode.numeric<-function(x,na.rm=TRUE){
   # vector
+  if(na.rm==TRUE){
+    x <- na.omit(x)
+  }
   x[which.max(sapply(seq_along(x), 
                        function(y) sum(x == x[y])))]
   
 }
 #' @export
-get_mode.character <- function(x){
+get_mode.character <- function(x, na.rm=TRUE){
   # vector
+  if(na.rm==TRUE){
+    x <- na.omit(x)
+  }
   x[which.max(sapply(seq_along(x), 
                        function(y) sum(x == x[y])))]
   
 }
 #' @export
-get_mode.default<-function (x){
-    x<-x[!is.na(x)]
-    y <- x
-   z <- vector("numeric")
-    for (j in 1:length(y)) {
-      z[j] <- sum(y == y[j])
-    }
-    q <- which(z == max(z))
-    res <- unique(y[q])
-    ifelse(length(res) >= 2, min(res), res)
+get_mode.default<-function (x,na.rm=TRUE){
+stop(paste0("No implementation available for objects of class
+             ", class(x)))  
   
-
-
-
 }
 
 #' @export
-get_mode.data.frame <- function(x,...){
+get_mode.data.frame <- function(x,na.rm=TRUE){
+  
+  
+if(any(sapply(x, class)=="factor")){
+ warning("factor columns have been removed.")
+x <-Filter(Negate(is.factor), x) 
+}
 
-  warning("factor columns have been removed.")
-
-mapply(get_mode,Filter(Negate(is.factor), x))
+if(na.rm==TRUE){
+  x <- na.omit(x)
+}
+structure(sapply(seq_along(x),
+         function(column) x[,
+                    column][which.max(apply(x[column],1,
+                                  function(y) 
+                              sum(x[column] ==y)))]),
+         names = names(x))
   
 
  
