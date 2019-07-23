dd6c8d231e779f28241088714dc8cde779925c2b
Nelson-Gon <gonzabato@hotmail.com>
2019-07-24 00:23:19 +0800

    numeric, character, s3

--- R/row_mean_na.R
+++ R/row_mean_na.R
@@ -31,9 +31,10 @@ row_mean_na<-function(data,func,observations,na.rm=F,exc){
 }
 }
 #' @title Replace missing values
-#' @param df The data set for which replacements are required
+#' @param df The data set(data.frame or vector) for which replacements are required
 #' @param how How should missing values be replaced?  One of ffill, samples,value or any other known
-#' method e.g mean, median, max ,min. The default is NULL meaning no  imputation is done. 
+#' method e.g mean, median, max ,min. The default is NULL meaning no  imputation is done. For character vectors,
+#' the use of `get_mode` is also supported. No implementation for class factor(yet).
 #' @param value If how is set to value, this allows the user to provide a specific fill value for the NAs.
 #' @details This function currently does not support grouping although this may be achieved with some
 #' inaccuracies using grouping functions from other packages.
@@ -46,7 +47,7 @@ na_replace<-function(df,how=NULL,value=NULL){
 }
 #' @export
 
-na_replace.default<-function(df,how=NULL,value=NULL){
+na_replace.data.frame<-function(df,how=NULL,value=NULL){
   if(is.null(how)){
     return(df)
   }
@@ -80,3 +81,74 @@ na_replace.default<-function(df,how=NULL,value=NULL){
   
   
 }
+na_replace.numeric<-function(df,how=NULL,value=NULL){
+  if(is.null(how)){
+    return(df)
+  }
+  
+  else if(!how%in%c("ffill","samples","value")){
+ replace(df,is.na(df), do.call(how,list((na.omit(df)))))
+                         
+  }
+  else{
+    if(how=="ffill"){
+      to_replace<-df[!is.na(df)]
+      replace(df, is.na(df),
+              to_replace[1:length(df[is.na(df)])])
+      
+    }
+    else if(how=="samples"){
+    
+  replace(df,is.na(df),
+              sample(na.omit(df),
+                     length(df[is.na(df)]),
+                     replace = TRUE))
+      }
+      
+    else if(how =="value"){
+     replace(df,is.na(df),value)
+    }
+    
+  }
+  
+  
+}
+
+na_replace.character<-function(df,how=NULL,value=NULL){
+  if(is.null(how)){
+    return(df)
+  }
+  
+  else if(!how%in%c("ffill","samples","value","get_mode")){
+   stop("how must be one of ffill, samples or value.Cannot do 
+        Maths on non-numerics")
+    
+  }
+  else{
+    if(how=="ffill"){
+      to_replace<-df[!is.na(df)]
+      replace(df, is.na(df),
+              to_replace[1:length(df[is.na(df)])])
+      
+    }
+    else if(how=="samples"){
+      
+      replace(df,is.na(df),
+              sample(na.omit(df),
+                     length(df[is.na(df)]),
+                     replace = TRUE))
+    }
+    
+    else if(how =="value"){
+      replace(df,is.na(df),value)
+    }
+    
+    else if(how=="get_mode"){
+    replace(df,is.na(df), do.call("get_mode",list((na.omit(df)))))
+    }
+    
+  }
+  
+  
+}
+
