834ddbc1394413670be98cc8801f9341b073012b
Nelson-Gon <gonzabato@hotmail.com>
2019-07-27 02:33:38 +0800

    colors, tough choice

--- R/plot_corr.R
+++ R/plot_corr.R
@@ -1,6 +1,6 @@
 #' Plot a correlations matrix
 #' @import ggplot2
-#' @description This function plots the results produced by 
+#' @description This function plots the results produced by
 #' `get_var_corr_`.
 #' @param data The data to be plotted. A `data.frame` object produced
 #' by `get_var_corr_`
@@ -9,15 +9,15 @@
 #' @param show_value Logical. Should correlation coefficients be shown on the plot?
 #' Defaults to TRUE.
 #' @param round_values Logical. Should values be rounded off? Defaults to TRUE.
-#' @param decimals     Numeric. To how many decimal places should the rounding be done? 
+#' @param decimals     Numeric. To how many decimal places should the rounding be done?
 #' Defaults to 2.
-#' @param custom_cols A vector(length 2) of colors to use for the plot. The first colour specifies the lower end of the 
+#' @param custom_cols A vector(length 2) of colors to use for the plot. The first colour specifies the lower end of the
 #' correlations. The second specifies  the higher end.
 #' @param title plot title.
 #' @param xlabel label for the x axis
 #' @param ylabel label for the y axis
 #' @param title_just Justification of the title. Defaults to 0.5, title is centered.
-#' @param plot_style One of squares and circles(currently). 
+#' @param plot_style One of squares and circles(currently).
 #' @param size Size of the circles for plot_style set to circles
 #' @param colour_by The column to use for coloring. Defaults to  "Correlation". Colour strength thus
 #' indicates the strength of correlations.
@@ -25,119 +25,148 @@
 #' @param value_col What colour should the text in the squares/circles be?
 #' @param value_angle What angle should the text be?
 #' @param value_size Size of the text.
+#' @param width width value for plot_style set to squares.
 #' @param ... Other arguments to specific methods.
-#' @details 
+#' @details
 #' This function uses `ggplot2` backend. `ggplot2` is thus required for the plots to work.
 #' @return A `ggplot2` object.
-#' @examples 
+#' @examples
 #' # compute correlations
 #' res<-get_var_corr_(mtcars)
-#' plot_corr(res, decimals = 2, plot_style = "squares",
-#' show_value = TRUE, size = 13,
-#' value_angle =20, colour_by = "Correlation")
+#' # defaults
+#' plot_corr(res)
+#' plot_corr(res, decimals = 2, plot_style = "circles",
+#' show_value = TRUE, size = 7,
+#' value_angle =360, colour_by = "Correlation",
+#' shape = 16,
+#' value_size = 3.5,
+#' width = 7.5,
+#' custom_cols = c("white","green2",
+#' "gray34"),
+#'              value_col = "black",
+#'              x="Other_Var",y="Comparison_Var",
+#'              xlabel = "Other_Variable",
+#'              ylabel="Comaprison_Variable")
 
-#' 
+#'
 #' @export
-#' 
-#' 
-plot_corr<-function(data,x="Comparison_Var",
-                            y="Other_Var",show_value=TRUE,
-                            round_values=TRUE,
-                            decimals = 2,
-                            custom_cols=c("skyblue",
-                                          "indianred2"),
-                            title="Correlations Plot",
-                            xlabel="Comparison_Variable",
-                            ylabel="Other_Variable",
-                            title_just = 0.5,
-                            plot_style = "squares",
-                            size= 12,
-                            colour_by = NULL,
-                            shape=NULL,
-                            value_col="blue",
-                            value_angle=360,
-                            value_size=4.5,
-                            
-                            ...){
+#'
+#'
+plot_corr <- function(data,
+                      x = "Comparison_Var",
+                      y = "Other_Var",
+                      xlabel = "Other_Variable",
+                      ylabel = "Comaprison_Variable",
+                      title = "Correlations Plot",
+                      plot_style = "circles",
+                      title_just = 0.5,
+                      round_values = TRUE,
+                      colour_by = "Correlation",
+                      decimals = 2,
+                      show_value = TRUE,
+                      size = 7,
+                      value_angle = 360,
+                      shape = 16,
+                      value_size = 3.5,
+                      value_col = "black",
+                      width = 7.5,
+                      custom_cols = c("white", "green2",
+                                      "gray34"),
+                      
+                      
+                      
+                      
+                      ...) {
   UseMethod("plot_corr")
 }
 #' @export
-plot_corr<- function(data,x="Comparison_Var",
-                     y="Other_Var",show_value=TRUE,
-                     round_values=TRUE,
-                     decimals = 2,
-                     custom_cols=c("skyblue",
-                                   "indianred2"),
-                     title="Correlations Plot",
-                     xlabel="Comparison_Variable",
-                     ylabel="Other_Variable",
-                     title_just = 0.5,
-                     plot_style = "squares",
-                     size= 12,
-                     colour_by = NULL,
-                     shape=NULL,
-                     value_col="blue",
-                     value_angle=360,
-                     value_size=4.5,
-                    
-                    ...){
+plot_corr <- function(data,
+                      x = "Comparison_Var",
+                      y = "Other_Var",
+                      xlabel = "Other_Variable",
+                      ylabel = "Comaprison_Variable",
+                      title = "Correlations Plot",
+                      plot_style = "circles",
+                      
+                      title_just = 0.5,
+                      round_values = TRUE,
+                      colour_by = "Correlation",
+                      decimals = 2,
+                      show_value = TRUE,
+                      size = 7,
+                      value_angle = 360,
+                      
+                      shape = 16,
+                      value_size = 3.5,
+                      value_col = "black",
+                      width = 7.5,
+                      custom_cols = c("white", "green2",
+                                      "gray34"),
+                      
+                      
+                      
+                      
+                      ...) {
   #visible binding
-  if(is.null(colour_by)){
+  if (is.null(colour_by)) {
     colour_by <- data$Correlation
   }
-
+  
   # Basic plot
-  if(round_values){
+  if (round_values) {
     data["Correlation"] <- round(data["Correlation"],
-                              decimals)
-  
-  }
-  base_plot<-ggplot2::ggplot(data=data,
-                  mapping = 
-            ggplot2::aes_string(x=x,
-                                y=y))
-if(plot_style == "squares"){
-  base_plot <- base_plot+
-               geom_tile(size=size,
-                         aes_string(fill=colour_by),
-                         
-                         ...)+
-    scale_fill_gradient(low=custom_cols[1],
-                        high=custom_cols[2],
-                        ...)
-}
-  
-else if(plot_style =="circles"){
-  base_plot <- base_plot+
-               geom_point(size=size,
-                          aes_string(col=colour_by),
-                          shape=shape,
-                          ...)+
-    scale_color_gradient(low=custom_cols[1], 
-                         high=custom_cols[2],
-                         ...)
-}
-if(show_value){
-  base_plot <- base_plot + 
-    geom_text(aes_string(label=colour_by),
-                  color=value_col,
-              angle=value_angle,size=value_size)
-}
- 
-  
-# set basic colours
-actual_plot<-base_plot
-# Themes 
-actual_plot+
-  labs(x=xlabel, y=ylabel, title = title)+
-  theme(plot.title = element_text(hjust=title_just,
-                                  ...),
-        panel.grid = element_blank(),
-        panel.background = element_blank())
-
-  
+                                 decimals)
     
+  }
+  base_plot <- ggplot2::ggplot(data = data,
+                               mapping =
+                                 ggplot2::aes_string(x = x,
+                                                     y = y))
+  if (plot_style == "squares") {
+    base_plot <- base_plot +
+      geom_tile(size = size,
+                aes_string(fill = colour_by),
+                width = width,
+                
+                ...) +
+      scale_fill_gradient2(low = custom_cols[1],
+                           mid = custom_cols[2],
+                           high = custom_cols[3])
+  }
+  
+  else if (plot_style == "circles") {
+    base_plot <- base_plot +
+      geom_point(size = size,
+                 aes_string(col = colour_by),
+                 shape = shape) +
+      scale_color_gradient2(low = custom_cols[1],
+                            high = custom_cols[2],
+                            mid = custom_cols[3])
+  }
+  if (show_value) {
+    base_plot <- base_plot +
+      geom_text(
+        aes_string(label = colour_by),
+        color = value_col,
+        angle = value_angle,
+        size = value_size
+      )
+  }
+  
+  
+  # set basic colours
+  actual_plot <- base_plot
+  # Themes
+  actual_plot +
+    theme_classic() +
+    labs(x = xlabel, y = ylabel, title = title) +
+    theme(
+      plot.title = element_text(hjust = title_just,
+                                ...),
+      panel.grid = element_blank(),
+      panel.background = element_blank()
+    )
+  
+  
+  
 }
-
-
-
