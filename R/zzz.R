1b2c95c63cd00043cfe8a8a8e828b3dbc7bd25c9
Nelson-Gon <gonzabato@hotmail.com>
2019-08-21 01:54:09 +0800

    startup

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+# Welcome messages, nothing fancy
+.onAttach <- function(...){
+  # better to paste or c the message
+  pkg_info <- "Welcome to manymodelr.
+For the latest development version,
+please see: https://www.github.com/Nelson-Gon/manymodelr.
+Please file issues at https://github.com/Nelson-Gon/manymodelr/issues"
+  packageStartupMessage(pkg_info)
+}
