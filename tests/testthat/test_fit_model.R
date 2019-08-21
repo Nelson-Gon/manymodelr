b3513c5d8061c29fb03af35de827a968e33f1e70
Nelson-Gon <gonzabato@hotmail.com>
2019-08-21 15:50:11 +0800

    rethinking model_fit

--- tests/testthat/test_fit_model.R
+++ tests/testthat/test_fit_model.R
@@ -3,12 +3,10 @@ library(manymodelr)
 testthat::test_that(desc="test fit_model",
                     code={
                       
-          iris1<-iris[1:60,]
-          iris2<-iris[60:nrow(iris),]
-    testthat::expect_error(
-            fit_model(iris1,Sepal.Length,Petal.Length,
-                               anova,na.rm=TRUE,iris2),
-        "Model Type Not Suitable.")
+       testthat::expect_equal(
+            class(fit_model(iris,"Sepal.Length","Petal.Length",
+                               "aov"))[1],
+        "aov", fixed = TRUE)
             
                     })
 
