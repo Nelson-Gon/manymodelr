682e6bda1a102f518106ab2d702fe02e2febcb11
Nelson-Gon <gonzabato@hotmail.com>
2025-03-12 00:19:46 -0400

    Dummy test

--- R/agg_by_group.R
+++ R/agg_by_group.R
@@ -1,7 +1,7 @@
 #' A convenient way to perform grouped operations
 #' @importFrom stats aggregate
 #' @description This function performs operations by grouping the data.
-#' @param df The data set for which correlations are required
+#' @param data_set The data set for which correlations are required
 #' @param my_formula A formula such as A~B where B is the grouping variable(normally a factor).
 #' See examples below
 #' @param  func The kind of operation e.g sum,mean,min,max,manymodelr::get_mode
@@ -10,27 +10,27 @@
 #' @examples
 #' head(agg_by_group(airquality,.~Month,sum))
 #' @export
-agg_by_group <- function(df=NULL,my_formula=NULL,func=NULL,...){
+agg_by_group <- function(data_set=NULL,my_formula=NULL,func=NULL,...){
   UseMethod("agg_by_group")
 }
 #' @export
-agg_by_group.default <- function(df=NULL,my_formula=NULL,func=NULL,...){
-  stop(paste0("Don't know how to deal with an object of class ", class(df)))
+agg_by_group.default <- function(data_set=NULL,my_formula=NULL,func=NULL,...){
+  stop(paste0("Don't know how to deal with an object of class ", class(data_set)))
 
 }
 #' @export
-agg_by_group.data.frame <-function(df=NULL,my_formula=NULL,func=NULL,...){
+agg_by_group.data.frame <-function(data_set=NULL,my_formula=NULL,func=NULL,...){
 
-  if(any(is.null(my_formula), is.null(func), is.null(df))){
+  if(any(is.null(my_formula), is.null(func), is.null(data_set))){
     stop("You should provide a data.frame, formula, and a function.")
   }
 
 
-    my_formula<-deparse(substitute(my_formula))
+  my_formula<-deparse(substitute(my_formula))
 
-  res<-aggregate(as.formula(my_formula),df,func,...)
-  print(res)
-  attr(res,"Groups")<-trimws(gsub("[~+]","",gsub(".*(?=~)","",my_formula,perl=TRUE),"left"))
+  res<-aggregate(as.formula(my_formula),data_set,func,...)
+  extracted_attr <- trimws(gsub("[~+]","",gsub(".*(?=~)","",my_formula,perl=TRUE),"left"))
+  attr(res,"Groups")<-extracted_attr
   attr(res,"Groups")<-strsplit(attributes(res)$Groups,"\\s+")[[1]]
   res_list<-unlist(strsplit(attributes(res)["Groups"][[1]]," "))
   res_list<-res_list[res_list!=""]
--- data-raw/yields.R
+++ data-raw/yields.R
@@ -2,10 +2,10 @@
 
 set.seed(520)
 # Create a simple dataset with a binary target
-# Here normal is a fictional target where we assume that it meets 
-# some criterion means 
-yields <- data.frame(normal = as.factor(rep(c("Yes", "No"), 500)), 
-                          height=rnorm(100, mean=0.5, sd=0.2), 
+# Here normal is a fictional target where we assume that it meets
+# some criterion means
+yields <- data.frame(normal = rep(c("Yes", "No"), 500),
+                          height=rnorm(100, mean=0.5, sd=0.2),
                           weight=runif(100,0, 0.6),
                           yield = rnorm(100, mean =520, sd = 10))
 
