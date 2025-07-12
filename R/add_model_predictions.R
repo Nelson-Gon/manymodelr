66d9b77b6ae5c8159e9e948685b6bdd0fcff27e7
Nelson-Gon <gonzabato@hotmail.com>
2025-07-12 12:27:35 -0400

    Init #24

--- R/add_model_predictions.R
+++ R/add_model_predictions.R
@@ -5,11 +5,11 @@
 #' @param new_data The data set to use for predicting.
 #' @return A data.frame object with a new column for predicted values
 #' @seealso \code{\link{fit_model}} \code{\link{extract_model_info}}
-#' @examples 
+#' @examples
 #' data("yields", package="manymodelr")
 #' yields1 <- yields[1:50,]
 #' yields2<- yields[51:100,]
-#' lm_model <- fit_model(yields1,"weight","height","lm") 
+#' lm_model <- fit_model(yields1,"weight","height","lm")
 #' head(add_model_predictions(lm_model,yields1,yields2))
 #' @export
 add_model_predictions<- function(model=NULL,old_data=NULL, new_data=NULL){
@@ -17,32 +17,31 @@ add_model_predictions<- function(model=NULL,old_data=NULL, new_data=NULL){
 }
 #' @export
 add_model_predictions<- function(model=NULL,old_data=NULL, new_data=NULL){
-  
+
   if(any(is.null(old_data), is.null(new_data), is.null(model))) stop("A model, old_data, and new_data must be provided")
-  
- 
+
+
   if(nrow(old_data)!=nrow(new_data)) stop("old_data and new_data must have equal lengths.")
-  
+
   predictions <- predict(model,new_data)
 
   old_data$predicted <- predictions
   old_data
 }
-
 #' Add model residuals
 #' @description A dplyr compatible convenience function to add residuals to a data set
 #' @inheritParams add_model_predictions
 #' @return A data.frame object with residuals added.
-#' @examples 
+#' @examples
 #' data("yields", package="manymodelr")
 #' yields1 <- yields[1:50,]
 #' yields2 <- yields[51:100,]
-#' lm_model <- fit_model(yields1,"weight","height","lm") 
+#' lm_model <- fit_model(yields1,"weight","height","lm")
 #' head(add_model_residuals(lm_model, yields2))
 #' @export
 add_model_residuals <- function(model=NULL, old_data=NULL){
 UseMethod("add_model_residuals")
-  
+
 }
 #' @export
 add_model_residuals <- function(model=NULL, old_data=NULL){
@@ -58,9 +57,9 @@ if (!is.data.frame(old_data)){
 old_data$residuals <- residuals(model)
 
 old_data
-  
 
- 
+
+
 }
 
 
