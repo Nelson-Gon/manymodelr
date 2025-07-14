24ee58256007a4bfcf4bf4ad369c6a733f428ca9
Nelson-Gon <gonzabato@hotmail.com>
2025-07-14 18:58:20 -0400

    #24 Update namespace generation

--- R/imports.R
+++ R/imports.R
@@ -1,25 +1,11 @@
-#' @importFrom stats aggregate stats coef residuals AIC na.omit cor.test
+#' @importFrom stats aggregate coef residuals AIC na.omit cor.test predict as.formula complete.cases setNames BIC logLik deviance df.residual
 #' @importFrom lme4 lmer
-#' @importFrom dplyr %>%
 #' @importFrom utils combn globalVariables packageVersion
-#' @importFrom dplyr "%>%" "arrange" "desc" "everything" "mutate" "summarise"
-#' "select" "pull" "filter" "group_by" "lead" "lag" "sym" "across"  "tibble"
-#'  "as_tibble"
-#' @importFrom stats "as.formula"  "complete.cases" "setNames" "na.omit" "predict"
-#' "AIC" "aggregate" "cor.test" "residuals" "BIC" "logLik" "deviance" "df.residual"
-#' @importFrom stringr "str_replace_all"
-#' @importFrom utils "combn" "globalVariables" "packageVersion"
-#' @importFrom usethis "use_data"
-#' @importFrom testthat "test_that" "skip"
-#' @importFrom lme4 "lmer"
-#' @importFrom caret "train"
-# @import e1071   e1071 (>= 1.7.8)
-#todo: allow override for more flexibility
-#' @importFrom Metrics  "rmse" "accuracy" "auc" "mae" "se" "ae" "f1" "sse" "rse"
-#' "rrse"
-#' @importFrom ggplot2 "ggplot" "aes" "geom_point" "scale_color_gradient2" "labs"
-#'  "geom_tile" "scale_fill_gradient2" "geom_text" "theme" "theme_minimal" "waiver"
-#'  "aes_string" "element_text" "element_blank" "ggplot_build"
-#' @importFrom dplyr mutate select everything
+#' @importFrom dplyr "%>%" arrange desc everything mutate summarise select pull filter group_by lead lag sym across tibble as_tibble
 #' @importFrom stringr str_replace_all
-
+#' @importFrom usethis use_data
+#' @importFrom testthat test_that skip
+#' @importFrom caret train trainControl confusionMatrix createDataPartition
+#' @importFrom Metrics rmse accuracy auc mae se ae f1 sse rse rrse
+#' @importFrom ggplot2 ggplot aes geom_point scale_color_gradient2 labs geom_tile scale_fill_gradient2 geom_text theme theme_minimal waiver aes_string element_text element_blank ggplot_build
+NULL
